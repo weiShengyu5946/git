8e84f88a5a16191f41c41fbf34d60e579883dcf9
weiShengyu5946 <1404356476@qq.com>
2020-12-01 17:50:47 +0800

    st

---
+++ st/src/st/stu.java
@@ -0,0 +1,23 @@
+package st;
+
+public class stu {
+	
+	 private int id;
+	 private String username;
+	 private String password;
+	 private int sex;
+	 private String love;
+
+
+	public stu(int id,String username,String password,int sex,String love ) {
+		
+		super();
+		this.id = id;
+		this.username = username;
+		this.password= password;
+	    this.sex=sex;
+		this.love=love;
+		
+	}
+
+}
